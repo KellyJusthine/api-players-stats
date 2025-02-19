c283d80d4c07f37bf3fd934312cbb5bf870ea4d5
KellyJusthine <kellyjusthine2003@gmail.com>
2025-02-19 13:20:33 -0300

    Initial commit

---
+++ src/main/java/api_players_stats/ApiPlayersStatsApplication.java
@@ -0,0 +1,13 @@
+package api_players_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlayersStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlayersStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api_players_stats/ApiPlayersStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package api_players_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlayersStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
